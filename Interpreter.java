f521e2142d4c166adf3797ae6d51beb393534be7
ayy-arsnl <aurquhar@uccs.edu>
2021-09-30 13:29:51 -0600

    added interpreter class

---
+++ Interpreter.java
@@ -0,0 +1,55 @@
+import ADT.*;
+
+public class Interpreter {
+    public boolean initializeFactorialTest(SymbolTable stable, QuadTable qtable){
+        return true;
+    }
+
+    public boolean initializeSummationTest(SymbolTable stable, QuadTable qtable){
+        return true;
+    }
+
+    public void InterpretQuads(QuadTable Q, SymbolTable S, boolean TraceOn, String filename){
+
+
+        int opcode = Q.GetQuad(Q.NextQuad(), 0);
+        switch(opcode){
+            case 0:
+
+            case 1:
+
+            case 2:
+
+            case 3:
+            
+            case 4:
+
+            case 5:
+
+            case 6:
+
+            case 7:
+
+            case 8:
+
+            case 9:
+
+            case 10:
+
+            case 11:
+
+            case 12:
+
+            case 13:
+
+            case 14:
+
+            case 15:
+
+            case 16:
+
+        }
+
+    }
+
+}
