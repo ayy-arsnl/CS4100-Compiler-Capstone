6234dcdb63c32ddde93d45bcde793caa97bc1d8d
ayy-arsnl <alex.ryan.urquhart@gmail.com>
2021-12-06 11:58:44 -0700

    Added flles for P4

---
+++ mainCODEGEN.java
@@ -0,0 +1,25 @@
+//import ADT.SymbolTable;
+//import ADT.Lexical;
+import java.io.IOException;
+
+import ADT.*;
+
+/**
+ *
+ * @author abrouill FALL 2021
+ */
+public class mainCODEGEN {
+
+    public static void main(String[] args) throws IOException{
+        String currentDir = System.getProperty("user.dir");
+        String filePath = currentDir + "/CodeGenBASIC.txt";
+        //String filePath = "d:\\BadSyntax-1-ASP21.txt";
+        System.out.println("Parsing "+filePath);
+        boolean traceon = false; //true; //false;
+        //Syntactic parser = new Syntactic(filePath, traceon);
+        //parser.parse();
+        
+        System.out.println("Done.");
+    }
+
+}
